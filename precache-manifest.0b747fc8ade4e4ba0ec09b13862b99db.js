4ab2b1c60024945afb2b2c247ca02218846d0d98
mzord <alvesefomm@gmail.com>
2019-06-12 18:18:08 -0300

    Updates

---
+++ precache-manifest.0b747fc8ade4e4ba0ec09b13862b99db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "021372e4c670ff5bcd5792b31012a6d9",
+    "url": "/AlvesPortfolio/index.html"
+  },
+  {
+    "revision": "b390f6f54fc1189f32b0",
+    "url": "/AlvesPortfolio/static/css/main.1e194975.chunk.css"
+  },
+  {
+    "revision": "fe0ff41f1d72f3fd26e0",
+    "url": "/AlvesPortfolio/static/js/2.829d31f3.chunk.js"
+  },
+  {
+    "revision": "b390f6f54fc1189f32b0",
+    "url": "/AlvesPortfolio/static/js/main.e9edb4f3.chunk.js"
+  },
+  {
+    "revision": "14ddfed8864451a1b8dc",
+    "url": "/AlvesPortfolio/static/js/runtime~main.b594c9d9.js"
+  },
+  {
+    "revision": "eaabf717fa6296c055dcd0a8927295ea",
+    "url": "/AlvesPortfolio/static/media/download.eaabf717.png"
+  }
+]);
